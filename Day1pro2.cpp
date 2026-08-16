5a39699c6ec34b4896560f38da57834b35969920
neerajmk009 <neerajmk009@gmail.com>
2026-08-16 22:23:25 +0530

    Update gitignore

---
+++ Day1pro2.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    string name = "Neeraj";
+    int age = 19;
+    float height = 6.2;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout<<"THIS IS MY DAY 1 PROBLEM 2 .\n My Name is "<<name<<".\n My Age is "<<age;
+    cout<<".\n My Height is "<<height;
+    cout<<".\n My Grade is "<<grade;
+    
+    
+    return 0;
+
+}
