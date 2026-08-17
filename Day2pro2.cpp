99e67ec2c91debe76d079dfb13f73f337309afc2
neerajmk009 <neerajmk009@gmail.com>
2026-08-17 21:42:51 +0530

    Day 2 - calculator with cin

---
+++ Day2pro2.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<<"Enter a Number";
+    cin>>num;
+
+    cout<<" remainder when divided by 2: "<<num%2 <<endl;
+
+    return 0;
+
+}
