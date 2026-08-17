f40261919866a040025ebc54acd3284ed838e47e
neerajmk009 <neerajmk009@gmail.com>
2026-08-17 21:32:11 +0530

    Remove tut5 from tracking

---
+++ tut6.cpp
@@ -0,0 +1,29 @@
+# include<iostream>
+
+using namespace std;
+ int main(){
+    int a = 5;
+    int b = 5;
+
+    cout<<"The value of a+b "<<a+b<<endl;
+    cout<<"The value of a-b "<<a-b<<endl;
+    cout<<"The value of a*b "<<a*b<<endl;
+    cout<<"The value of a/b "<<a/b<<endl;
+    cout<<"The value of a%b "<<a%b<<endl;
+    cout<<"The value of a++ "<<a++<<endl;
+    cout<<"The value of a-- "<<a--<<endl;
+    cout<<"The value of ++a "<<++a<<endl;
+    cout<<"The value of --a "<<--a<<endl;
+
+    
+    cout<<"This is value of a=b"<<(a=b)<<endl;
+    cout<<"This is value of a!=b"<<(a!=b)<<endl;
+    cout<<"This is value of a<b"<<(a<b)<<endl;
+    cout<<"This is value of a>b"<<(a>b)<<endl;
+    cout<<"This is value of a<=b"<<(a<=b)<<endl;
+    cout<<"This is value of a>=b"<<(a>=b)<<endl;
+
+
+
+    return 0;
+ }
