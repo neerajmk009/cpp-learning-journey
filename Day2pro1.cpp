25d521c86611402a94abdec29280343ca2062722
neerajmk009 <neerajmk009@gmail.com>
2026-08-17 21:29:02 +0530

    Day 2 - calculator with cin

---
+++ Day2pro1.cpp
@@ -0,0 +1,21 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+
+    float num1, num2;
+
+    cout<<"Enter the value of num1:\n";
+    cin>>num1;
+
+    cout<<"Enter the value of num2:\n";
+    cin>>num2;
+
+    cout<< "Sum of this: "<<num1+num2<<endl;
+    cout<< "subtract of this: "<<num1-num2<<endl;
+    cout<< "multiply of this: "<<num1*num2<<endl;
+    cout<< "division of this: "<<num1/num2<<endl;
+
+    return 0;
+    
+}
