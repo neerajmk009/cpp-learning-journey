cd7189097168bf10dc911c9613659f84913415e7
neerajmk009 <neerajmk009@gmail.com>
2026-08-17 22:03:04 +0530

    Day 2 - calculator with cin

---
+++ Day2pro3.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int x , y;
+
+    cout<<" Enter the value of x: "<< endl;
+    cin>>x;
+
+    cout<<" Enter the value of y: "<< endl;
+    cin>>y;
+
+    cout<<" x==y "<< (x==y) << endl;
+    cout<<" x<y "<< (x<y) << endl;
+    cout<<" x>y "<< (x>y) << endl;
+    cout<<" x<=y "<< (x<=y) <<endl;
+    cout<<" x>=y "<< (x>=y) <<endl;
+    cout<<" x!=y "<< (x!=y) <<endl;
+
+    return 0;
+    
+}
